5998b801e6e999e6d46499118f6069c5ca259bfe
Evaldo Vega Vivanco <evaldo.vega@gmail.com>
2022-10-13 15:22:58 -0500

    x axis

--- App.js
+++ App.js
@@ -1,26 +1,47 @@
 import React, {useEffect, useRef} from 'react';
-import {View} from 'react-native';
-import Osciloscopio from './Osciloscopio/index';
+import {TextInput, View, Dimensions} from 'react-native';
+import Osciloscopio, {interpolar} from './Osciloscopio/index';
+import {
+  Canvas,
+  Circle,
+  useTouchHandler,
+  useValue,
+  Box,
+  rrect,
+  rect,
+} from '@shopify/react-native-skia';
+const ancho_pantalla = Dimensions.get('screen').width;
 
 const App = () => {
   const intervalo = useRef(null);
   const data_ble = useRef([]);
+  const data_ble_time = useRef(0);
   const data_ble_2 = useRef([]);
   const radians = useRef(0.0);
+
+  const cx = useValue(0);
+  const cy = useValue(0);
+
+  const touchHandler = useTouchHandler({
+    onActive: ({x, y}) => {
+      _y.current = interpolar(x, 0, -1, ancho_pantalla, 1);
+      cx.current = x + 16;
+      cy.current = y;
+    },
+  });
 
   function randomIntFromInterval(min, max) {
     // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  const simunlarBle = () => {
-    if (data_ble.current.length < 10000) {
-      const y = Math.sin(radians.current * Math.PI);
-      data_ble.current.push({y});
+  const _y = useRef(0);
 
-      const y_aletoria = randomIntFromInterval(-1, 1);
-      data_ble_2.current.push({y: y_aletoria});
-    }
+  const simunlarBle = () => {
+    //const y = Math.sin(radians.current * Math.PI);
+    const y = 0;
+    data_ble.current.push({y: _y.current, time: data_ble_time.current});
+    data_ble_time.current += 1;
 
     radians.current += 0.05;
     if (radians.current >= 2.0) {
@@ -29,6 +50,8 @@ const App = () => {
   };
 
   useEffect(() => {
+    data_ble_time.current = 0;
+    data_ble.current = [];
     intervalo.current = setInterval(simunlarBle, 10);
 
     return () => {
@@ -41,6 +64,11 @@ const App = () => {
   return (
     <View style={{flex: 1, backgroundColor: 'white'}}>
       <Osciloscopio lineas={lineas} escala_x={2} />
+      <Canvas
+        style={{with: '100%', height: 100, backgroundColor: 'silver'}}
+        onTouch={touchHandler}>
+        <Circle cx={cx} cy={32} r={32} color="red" />
+      </Canvas>
     </View>
   );
 };
--- Osciloscopio/index.js
+++ Osciloscopio/index.js
@@ -8,9 +8,13 @@ import {
   StrokeJoin,
   StrokeCap,
   useFont,
+  useValue,
+  rrect,
+  rect,
 } from '@shopify/react-native-skia';
 
-import {Dimensions, Text} from 'react-native';
+import {Dimensions, Text, useColorScheme} from 'react-native';
+import {assertTSExpressionWithTypeArguments} from '../../../../AppData/Local/Microsoft/TypeScript/4.8/node_modules/@babel/types/lib/index';
 
 const ancho_pantalla = Dimensions.get('screen').width;
 const alto_pantalla = Dimensions.get('screen').height;
@@ -20,20 +24,7 @@ paint.setAntiAlias(true);
 paint.setBlendMode(BlendMode.Multiply);
 const font_size = 8;
 
-export const interpolar = (
-  valor_a_calcular,
-  rango_inferior,
-  valor_rango_inferior,
-  rango_superiror,
-  valor_rango_superior,
-) => {
-  return (
-    ((valor_a_calcular - rango_inferior) *
-      (valor_rango_superior - valor_rango_inferior)) /
-      (rango_superiror - rango_inferior) +
-    valor_rango_inferior
-  );
-};
+console.log(rect(0, 0, 100, 100));
 
 const Osciloscopio = ({
   lineas = [],
@@ -45,9 +36,17 @@ const Osciloscopio = ({
   y_max = 1,
   color_ejes = '#A3A3A3',
   color_linea = '#59BBAB',
-  escala_x = 0.5,
 }) => {
+  const theme = useColorScheme();
   const font = useFont(require('./Fonts/Lato-Regular.ttf'), font_size);
+  //50ms
+  const velocidad_desplazamiento = useValue(5);
+  const puntos_eliminados = useValue(0);
+
+  const frecuencia = 10;
+  const divisiones_x = 4;
+  const ancho_segundo = (ancho_grafica - padding_horizontal) / divisiones_x;
+  const escala_x = ancho_segundo / (1000 / frecuencia);
 
   const stroke = useMemo(() => {
     const stroke = paint.copy();
@@ -58,25 +57,44 @@ const Osciloscopio = ({
     stroke.setStrokeJoin(StrokeJoin.Round);
     stroke.setColor(Skia.Color(color_ejes));
     return stroke;
-  }, [color_ejes]);
+  }, [color_ejes, theme]);
+
+  const stroke_bg = useMemo(() => {
+    const stroke_linea = paint.copy();
+    stroke_linea.setColor(Skia.Color(color_linea));
+    stroke_linea.setStyle(PaintStyle.Stroke);
+    stroke_linea.setStrokeJoin(StrokeJoin.Miter);
+    stroke_linea.setStrokeWidth(2);
+    return stroke_linea;
+  }, [color_linea, theme]);
 
   const stroke_linea = useMemo(() => {
     const stroke_linea = paint.copy();
     stroke_linea.setColor(Skia.Color(color_linea));
     stroke_linea.setStyle(PaintStyle.Stroke);
     stroke_linea.setStrokeJoin(StrokeJoin.Miter);
-    stroke_linea.setStrokeWidth(1);
+    stroke_linea.setStrokeWidth(2);
     return stroke_linea;
-  }, [color_linea]);
+  }, [color_linea, theme]);
 
   const stroke_linea_clara = useMemo(() => {
+    const color = theme == 'dark' ? 'white' : color_linea + '80';
+
     const stroke_linea = paint.copy();
-    stroke_linea.setColor(Skia.Color(color_linea + '80'));
+    stroke_linea.setColor(Skia.Color(color));
     stroke_linea.setStyle(PaintStyle.Stroke);
     stroke_linea.setStrokeJoin(StrokeJoin.Miter);
     stroke_linea.setStrokeWidth(1);
     return stroke_linea;
-  }, [color_linea]);
+  }, [color_linea, theme]);
+
+  const bg = useMemo(() => {
+    const bg = paint.copy();
+    const color = theme == 'dark' ? '#566573' : '#FDFEFE';
+    bg.setStyle(PaintStyle.Fill);
+    bg.setColor(Skia.Color(color));
+    return bg;
+  }, [theme]);
 
   const dibujarEjes = canvas => {
     const eje_y = Skia.Path.Make();
@@ -131,17 +149,47 @@ const Osciloscopio = ({
     }
   };
 
+  const dibujarDivisionesEnX = canvas => {
+    const y = alto_grafica + padding_vertical * 2;
+    const total_divisiones =
+      Math.round(lineas[0].current.length / divisiones_x) * frecuencia;
+
+    for (let i = 1; i < total_divisiones; i++) {
+      const division = Skia.Path.Make();
+      const x = i * ancho_segundo + puntos_eliminados.current * escala_x;
+      if (x < padding_horizontal) {
+        continue;
+      }
+      if (x > ancho_grafica) {
+        break;
+      }
+      const y = alto_grafica - padding_vertical * 2;
+      division.moveTo(x, y - 5);
+      division.lineTo(x, y + 5);
+      canvas.drawPath(division, stroke_linea);
+      canvas.drawText(i.toFixed(0) + 's', x - 2, y + 15, stroke, font);
+      const linea_vertical = Skia.Path.Make();
+      linea_vertical.moveTo(x, padding_vertical);
+      linea_vertical.lineTo(x, y);
+      canvas.drawPath(linea_vertical, stroke_linea_clara);
+    }
+  };
+
   const dibujarLineas = canvas => {
     for (let linea of lineas) {
       if (
         linea.current.length * escala_x >
         ancho_grafica - padding_vertical * 2
       ) {
-        linea.current = linea.current.splice(1);
+        linea.current = linea.current.splice(velocidad_desplazamiento.current);
+        puntos_eliminados.current -= velocidad_desplazamiento.current;
       }
+
       const line = Skia.Path.Make();
+
       for (let [i, punto] of linea.current.entries()) {
         const x = padding_horizontal + i * escala_x;
+
         if (x + padding_horizontal > ancho_grafica) {
           break;
         }
@@ -164,12 +212,23 @@ const Osciloscopio = ({
 
   const onDraw = useDrawCallback(
     canvas => {
+      canvas.save();
+      canvas.drawRect(rect(0, 0, ancho_grafica, alto_grafica), bg);
       dibujarLineas(canvas);
       dibujarEjes(canvas);
       dibujarDivisionesEnY(canvas);
+      dibujarDivisionesEnX(canvas);
+      canvas.restore();
     },
-    [font],
+    [font, theme],
   );
+
+  useEffect(() => {
+    puntos_eliminados.current = 0;
+    lineas.forEach(linea => {
+      linea.current = [];
+    });
+  });
 
   if (!font) {
     return <Text>Font no found!</Text>;
@@ -178,10 +237,59 @@ const Osciloscopio = ({
   return (
     <SkiaView
       mode="continuous"
-      style={{width: ancho_grafica, height: alto_grafica}}
+      style={{
+        width: ancho_grafica,
+        height: alto_grafica,
+      }}
       onDraw={onDraw}
     />
   );
 };
 
+export const interpolar = (
+  valor_a_calcular,
+  rango_inferior,
+  valor_rango_inferior,
+  rango_superiror,
+  valor_rango_superior,
+) => {
+  return (
+    ((valor_a_calcular - rango_inferior) *
+      (valor_rango_superior - valor_rango_inferior)) /
+      (rango_superiror - rango_inferior) +
+    valor_rango_inferior
+  );
+};
+
+function invertColor(hex, bw) {
+  if (hex.indexOf('#') === 0) {
+    hex = hex.slice(1);
+  }
+  // convert 3-digit hex to 6-digits.
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  if (hex.length !== 6) {
+    throw new Error('Invalid HEX color.');
+  }
+  var r = parseInt(hex.slice(0, 2), 16),
+    g = parseInt(hex.slice(2, 4), 16),
+    b = parseInt(hex.slice(4, 6), 16);
+  if (bw) {
+    // https://stackoverflow.com/a/3943023/112731
+    return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? '#000000' : '#FFFFFF';
+  }
+  // invert color components
+  r = (255 - r).toString(16);
+  g = (255 - g).toString(16);
+  b = (255 - b).toString(16);
+  // pad each with zeros and return
+  return '#' + padZero(r) + padZero(g) + padZero(b);
+}
+
+function padZero(str, len) {
+  len = len || 2;
+  var zeros = new Array(len).join('0');
+  return (zeros + str).slice(-len);
+}
 export default Osciloscopio;
